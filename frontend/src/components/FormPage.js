e47994fd762291c3c72b663ac4c4f71ee06975fb
Roman Beskokotov <roma@gmail.com>
2025-10-07 21:03:34 +0300

    Добавил компоненты FormPage с базовыми стилями

--- frontend/src/components/FormPage.js
+++ frontend/src/components/FormPage.js
@@ -1,4 +1,16 @@
-function FormPage() {
-    return ( <h1>Forms</h1> );
+import '../css/Form.css'
+
+const FormPage = () => {
+    return (
+        <main className="main_form_page">
+            <article> 3D MODEL </article>
+            <article>
+                <p>sdafasdfsdsfgsdfgsdfsdfgsdfgd</p>
+                <p>sdaflksdfoasd</p>
+                <button>dsafsdf</button>
+                <button>sdfxcvz</button>
+            </article>
+        </main>
+    );
 }
 export default FormPage
