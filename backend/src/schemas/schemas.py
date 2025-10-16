3693b13ab5ae59b11450bd820b9a0813159c7b01
Roman Beskokotov <roma@gmail.com>
2025-10-16 15:27:41 +0300

    Добавил документацию для схем pydantic

--- backend/src/schemas/schemas.py
+++ backend/src/schemas/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr
 
 class UsersAddDTO(BaseModel):
+    '''Pydantic схема для добавления нового пользователя'''
     username: str
     email: EmailStr
     password: str
@@ -8,11 +9,13 @@ class UsersAddDTO(BaseModel):
         return { 'username': self.username, 'email': self.email, 'password': self.password }
     
 class UsersDTO(UsersAddDTO):
+    '''Pydantic схема для вывода существующего пользователя'''
     id: int
     def get_attrs(self):
         attrs = super().get_attrs()
         attrs.update(id =self.id)
 
 class UserLoginDTO(BaseModel):
+    '''Pydantic схема для валидации логина и пароля'''
     username: str
     password: str
