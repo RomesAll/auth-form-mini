a0edac2258c4c71ea24b300e020d12652650fc7e
Roman Beskokotov <roma@gmail.com>
2025-10-16 15:38:01 +0300

    Добавил документацию для config.py

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,11 +1,12 @@
-from pydantic import field_validator, ValidationError
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from authx import AuthX, AuthXConfig
 
 class ConfigBase(BaseSettings):
+    '''Базовый класс ConfigBase, в котором происходит чтение данных из env файла'''
     model_config = SettingsConfigDict(env_file='backend/.env')
 
 class DataBase(ConfigBase):
+    '''Класс с конфигурацией базы данных (наследуется от ConfigBase), в котором переменные заполняются данными из env файла'''
     POSTGRES_HOST: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
@@ -14,9 +15,11 @@ class DataBase(ConfigBase):
 
     @property
     def DATABASE_URL_async(self):
+        '''Функция для вывода строки подключения к базе данных'''
         return f'postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}'
 
 class Authx(BaseSettings):
+    '''Класс с конфигурацией token JWT auth'''
     config: AuthXConfig = AuthXConfig()
     config.JWT_ACCESS_COOKIE_NAME = 'access_token'
     config.JWT_SECRET_KEY = 'SECRET'
@@ -24,6 +27,7 @@ class Authx(BaseSettings):
     security: AuthX = AuthX(config=config)
 
 class Settings(BaseSettings):
+    '''Класс для хранение конфигурации бд и token JWT auth'''
     database: DataBase = DataBase()
     authx: Authx = Authx()
 
