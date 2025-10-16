ffca7d0dfa7ab14fa29c19f4e7825c486138c65c
Roman Beskokotov <roma@gmail.com>
2025-10-17 00:14:22 +0300

    Добавил докер файл для react frontend

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -4,7 +4,7 @@ from authx import AuthX, AuthXConfig
 
 class ConfigBase(BaseSettings):
     '''Базовый класс ConfigBase, в котором происходит чтение данных из env файла'''
-    model_config = SettingsConfigDict(env_file='backend/.env', extra='ignore')
+    model_config = SettingsConfigDict(env_file='.env', extra='ignore')
 
 class DataBase(ConfigBase):
     '''Класс с конфигурацией базы данных (наследуется от ConfigBase), в котором переменные заполняются данными из env файла'''
